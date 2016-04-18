e682740151d1416b6cc44a4c95e2aea804d80e10
Warren Bain <warren@thoughtcroft.com>
2016-04-18 12:49:50 +1000

    End chapter 4

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -1,3 +1,26 @@
-Given(/^I am not yet playing$/) do
+class Output
+  def messages
+    @messages ||= []
+  end
 
+  def puts(message)
+    messages << message
+  end
+end
+
+def tty_output
+  @output ||= Output.new
+end
+
+
+Given(/^I am not yet playing$/) do
+end
+
+When(/^I start a new game$/) do
+  game = Codebreaker::Game.new(tty_output)
+  game.start
+end
+
+Then(/^I should see "([^"]*)"$/) do |message|
+  expect(tty_output.messages).to include(message)
 end
