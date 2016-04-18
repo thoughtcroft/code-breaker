b44cf1396c3c471f5223b340dc99f2cc9d872b2f
Warren Bain <warren@thoughtcroft.com>
2016-04-18 13:48:21 +1000

    End chapter 6

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -18,9 +18,22 @@ end
 
 When(/^I start a new game$/) do
   game = Codebreaker::Game.new(tty_output)
-  game.start
+  game.start('1234')
 end
 
 Then(/^I should see "([^"]*)"$/) do |message|
   expect(tty_output.messages).to include(message)
+end
+
+Given(/^the secret code is "([^"]*)"$/) do |secret|
+  @game = Codebreaker::Game.new(tty_output)
+  @game.start(secret)
+end
+
+When(/^I guess "([^"]*)"$/) do |guess|
+  @game.guess(guess)
+end
+
+Then(/^the mark should be "([^"]*)"$/) do |mark|
+  expect(tty_output.messages).to include(mark)
 end
